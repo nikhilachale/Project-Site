528e19c96abdbc9ce97b9f90023c56e0fd951bdb
[Anshu] <95041038+AnshuMishra01@users.noreply.github.com>
2022-07-06 13:27:21 +0530

    main

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCxFXL5zGD8OkHs83rc6RPhUY4XbvOxCls",
+    authDomain: "educational-site-65e32.firebaseapp.com",
+    projectId: "educational-site-65e32",
+    storageBucket: "educational-site-65e32.appspot.com",
+    messagingSenderId: "227655214336",
+    appId: "1:227655214336:web:af4f912821752c32587fe5",
+    measurementId: "G-1KHHLWPDKF"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
--- js/index.js
+++ js/index.js
@@ -1,38 +1,38 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-analytics.js";
+document.getElementById("loginForm").addEventListener("submit",(event)=>{
+    event.preventDefault()
+})
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCxFXL5zGD8OkHs83rc6RPhUY4XbvOxCls",
-  authDomain: "educational-site-65e32.firebaseapp.com",
-  projectId: "educational-site-65e32",
-  storageBucket: "educational-site-65e32.appspot.com",
-  messagingSenderId: "227655214336",
-  appId: "1:227655214336:web:af4f912821752c32587fe5",
-  measurementId: "G-1KHHLWPDKF"
-};
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-firebase.auth.Auth.Persistence.LOCAL;
-
-$("#btn-login").click(function()
-{
-    var email = $("#email").val();
-    var password = $("#password").val();
-
-    if(email != "" && password != ""){
-        var result = firebase.auth().signInWithEmailAndPassword();
-
-        result.catch(function(error){
-            var errorCode = error.code;
-            var errorMesage = error.message;
-
-            window.alert("Message : " + errorMessage);
-
-        });
+firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+        location.replace("welcome.html")
     }
-    else{
-        window.alert("please fill out all fields.");
-    }
+})
+
+function login(){
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch((error)=>{
+        document.getElementById("error").innerHTML = error.message
+    })
 }
-)
+
+function signUp(){
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .catch((error) => {
+        document.getElementById("error").innerHTML = error.message
+    });
+}
+
+function forgotPass(){
+    const email = document.getElementById("email").value
+    firebase.auth().sendPasswordResetEmail(email)
+    .then(() => {
+        alert("Reset link sent to your email id")
+    })
+    .catch((error) => {
+        document.getElementById("error").innerHTML = error.message
+    });
+}
